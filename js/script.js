c6cdc68814526c28c6d31289563d8ad143e493bf
Claudia Gutenberg <ca222ps@student.lnu.se>
2023-04-16 16:13:41 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Globala konstanter och variabler
+const wordList = ["BLOMMA", "LASTBIL", "SOPTUNNA", "KÖKSBORD", "RADIOAPPARAT", "VINTER", "SOMMAR", "DATORMUS", "LEJON", "ELEFANTÖRA", "JULTOMTE", "SKOGSHYDDA", "BILNUMMER", "BLYERTSPENNA", "SUDDGUMMI", "KLÄDSKÅP", "VEDSPIS", "LJUSSTAKE", "SKRIVBORD", "ELDGAFFEL", "STEKPANNA", "KASTRULL", "KAFFEBRYGGARE", "TALLRIK", "SOFFBORD", "TRASMATTA", "FLYGPLAN", "FLYGPLATS", "TANGENTBORD"]; // Lista (array) med ord som ska väljas slumpmässigt
+
+
+// --------------------------------------------------
+// Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
+// Initiering av globala variabler samt koppling av funktioner till knapparna.
+function init() {
+	
+} // Slut init
+window.onload = init; // Se till att init aktiveras då sidan är inladdad
+// --------------------------------------------------
+
