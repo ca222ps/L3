1901939ece9f18110d1cb153f921a2cea8e88fbc
Claudia Gutenberg <ca222ps@student.lnu.se>
2023-04-19 15:53:07 +0200

    1

--- js/script.js
+++ js/script.js
@@ -6,8 +6,24 @@ const wordList = ["BLOMMA", "LASTBIL", "SOPTUNNA", "KÖKSBORD", "RADIOAPPARAT", 
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
 // Initiering av globala variabler samt koppling av funktioner till knapparna.
 function init() {
-	
+    startGameBtn = document.getElementById("startGameBtn");
+    startGameBtn.onclick = startGame;
+    letterButtons = document.getElementById("letterButtons").getElementsByTagName("button");
+    for (let i = 0; i < letterButtons.length; i++)
+        letterButtons[i].onclick = guessLetter;
+    hangmanImg = document.getElementById("hangman");
+    msgElem = document.getElementById("message");
+    startGameBtn.disabled = false;
+        for (let i = 0; i < letterButtons.length; i++)
+            letterButtons[i].disabled = true; 
 } // Slut init
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 // --------------------------------------------------
-
+// Initiering av ny spelrunda. Val av ord, visa bokstavsrutor, visa första bilden (tom bild), sätt bildnummer till 0 , inaktivera startknappt och aktivera bokstavsknappar
+function startGame() {
+    radnomWord ();
+    showLetterBoxes();
+    hangmanImg.src = "img/h0.png";
+    hangmanNr = 0;
+	
+}
