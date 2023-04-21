c868e0180d7c34afbaafb29d9d16ebd3f9262fae
Claudia Gutenberg <ca222ps@student.lnu.se>
2023-04-21 17:17:19 +0200

    4

--- js/script.js
+++ js/script.js
@@ -5,21 +5,19 @@ var letterBoxes;    // Array med referenser till span-elementen för bokstävern
 var hangmanImg;     // Referens till img-elementet med bilder för galgen och gubben
 var hangmanNr;      // Nummer för aktuell bild (0-6), för bildfil som visas (så man sedan kan veta vilket som blir nästan bild)
 var msgElem;        // Referens till html div-elementet för skrivna meddelanden till användaren
-var startGameBtn;   // Referens till startknappen
-var letterButtons;  // Array med referenser till de span-taggar som utgör rutor för bokstäverna i ordet
 var startTime;      // Tid då spelet startas
 
 // --------------------------------------------------
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
 // Initiering av globala variabler samt koppling av funktioner till knapparna.
 function init() {
-    startGameBtn = document.getElementById("startGameBtn");
-    startGameBtn.onclick = startGame;
-    letterButtons = document.getElementById("letterButtons").getElementsByTagName("button");
+    let startGameBtn = document.getElementById("startGameBtn"); // Referens till startknappen
+    startGameBtn.onclick = startGame; // Funktionen startGame anropas vid klick på knappen
+    let letterButtons = document.getElementById("letterButtons").getElementsByTagName("button"); // Array med referenser till de span-taggar som utgör rutor för bokstäverna i ordet
     for (let i = 0; i < letterButtons.length; i++)
         letterButtons[i].onclick = guessLetter;
-    hangmanImg = document.getElementById("hangman");
-    msgElem = document.getElementById("message");
+    hangmanImg = document.getElementById("hangman"); // Referens till img-elementet "hangman"
+    msgElem = document.getElementById("message"); // Referens till div-elementet message
     startGameBtn.disabled = false;
         for (let i = 0; i < letterButtons.length; i++)
             letterButtons[i].disabled = true; 
@@ -28,10 +26,8 @@ window.onload = init; // Se till att init aktiveras först när sidan är inladd
 // --------------------------------------------------
 // Initiering av ny spelrunda. Val av ord, visa bokstavsrutor, visa första bilden (tom bild), sätt bildnummer till 0 , inaktivera startknappt och aktivera bokstavsknappar.
 function startGame() {
-    radnomWord ();
-    showLetterBoxes();
-    hangmanImg.src = "img/h0.png";
-    hangmanNr = 0;
+    hangmanImg.src = "img/h0.png"; 
+    hangmanNr = 0; // Variabeln hangmanNr tilldelas värdet 0 vid byte av bild
 } // Slut startGame
 
 function guessLetter() {
@@ -39,13 +35,14 @@ function guessLetter() {
 } // Slut guessLetter
 
 function randomWord () {
-
+    selectedWord
+// (Variabeln selectedWord tilldelas ett nytt värde i funktionen randomWord) selectedWord =
 } // Slut randomWord
 
 function showLetterBoxes() {
-
+// (Variabeln letterBoxes tilldelas i funktionen showLetterBoxes, då span-elementen lagts in i HTML-koden). letterBoxes = 
 } // Slut showLetterBoxes
 
-function endGame() {
+function endGame(manHanged) {
 
 } // Slut endGame
